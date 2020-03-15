2d6a267b5063a385f6de63ba9fcf45c650bd25e0
Hector Cen <hector.cen@gmail.com>
2020-03-15 00:30:59 -0400

    added static assets

--- app/mod_dashboard/controllers.py
+++ app/mod_dashboard/controllers.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, redirect, url_for, abort, render_template, jsonify
+from flask import Blueprint, redirect, url_for, abort, render_template
 
 # Define the blueprint
 entry_point = Blueprint('dashboard', __name__)
@@ -12,8 +12,8 @@ def index():
         raise e
         abort(404)
 
+
 @entry_point.route('/dashboard', methods=['GET'])
 def dashboard():
     # js = render_template('dashboard/index.js')
-    # return render_template('dashboard/index.html', title='Dashboard', user=current_user, js=js)
-    return jsonify({'test': 'Hello, World!'})
+    return render_template('dashboard/index.html', title='Dashboard')
