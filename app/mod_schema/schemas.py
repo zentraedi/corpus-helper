ab1f4c486ecaa7d386f487648deec61c79c8fee3
Hector Cen <hector.cen@gmail.com>
2020-03-16 23:57:45 -0400

    added mod_schemas

---
+++ app/mod_schema/schemas.py
@@ -0,0 +1,8 @@
+"""List of schemas to query in database
+"""
+
+schemas = [
+    'criterio1', 'criterio1b', 'criterio3', 'criterio3b', 'criterio5',
+    'criterio5b', 'criterio6', 'criterio7', 'criterio8', 'criterio24',
+    'criterio25', 'criterio30', 'criterio30b'
+]
